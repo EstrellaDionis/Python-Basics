be39a13629bdc54b395c01c13447201f75154539
EstrellaDionis <EstrellaDionis@gmail.com>
2021-07-28 18:07:22 -0400

    Account checker to help gauge how much you need

---
+++ bank_account_checker.py
@@ -0,0 +1,12 @@
+bank_account = 2
+
+if bank_account < 100000:
+    difference_to_100k = 100000 - bank_account
+    minimum_amount = difference_to_100k + bank_account
+    print(f"Here's some money, Sport:{difference_to_100k}")
+elif bank_account == 100000:
+    print("You have One Hundred Thousand Dollars")
+elif bank_account > 150000:
+    print("You're rich! oh and you're getting taxed")
+else:
+    print("You're sitting pretty my friend")
