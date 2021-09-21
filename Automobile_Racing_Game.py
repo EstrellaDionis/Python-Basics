4834d810b5a4d8bc391bb1b15ff63f3cdba7eab9
EstrellaDionis <EstrellaDionis@gmail.com>
2021-09-20 20:07:55 -0400

    creating base movement for a racing game

---
+++ Automobile_Racing_Game.py
@@ -0,0 +1,24 @@
+class Automobile:
+    def go(self):
+        print("Moving Forward")
+
+    def stop(self):
+        print("Stopping")
+
+
+class Motorcycle(Automobile):
+    def wheelie(self):
+        print("Popping a wheelie")
+
+
+class Racecar(Automobile):
+    def reverse(self):
+        print("Reversing")
+
+    def goTo100(self):
+        print("Increasing to 100 mph")
+
+
+motorcycle = Motorcycle()
+motorcycle.go()
+motorcycle.wheelie()
